4c778e9716f3dfbc8d51c7a0bb5f0099d3b5dc8d
oakencore <105373906+oakencore@users.noreply.github.com>
2024-03-19 16:49:07 -0400

    "Added validation styling"

--- script.js
+++ script.js
@@ -90,6 +90,8 @@ mainDiv.style.left = "0";
 // Fixed takes maindiv out of the normal document flow and positions it relative to the browser window
 mainDiv.style.position = "fixed";
 
+// Validation Styling
+
 function performValidation() {
   // input fields
   const email = document.getElementById("email");
@@ -98,8 +100,11 @@ function performValidation() {
 
   //Email validation
   if (email.validity.typeMismatch) {
+    email.style.border = "1px solid red";
+    email.nextElementSibling.style.display = "block";
     email.setCustomValidity("NEED AN EMAIL.");
   } else {
+    email.style.border = "1px solid #ccc";
     email.setCustomValidity("");
   }
 
@@ -108,9 +113,12 @@ function performValidation() {
     password.setCustomValidity(
       "NEED A PASSWORD THAT IS AT LEAST 8 CHARACTERS LONG."
     );
+    password.style.border = "1px solid red";
   } else if (password.value.trim() !== confirmPassword.value.trim()) {
+    password.style.border = "1px solid red";
     password.setCustomValidity("YOUR PASSWORDS DON'T MATCH");
   } else {
+    password.style.border = "1px solid #ccc";
     password.setCustomValidity("");
   }
 }
